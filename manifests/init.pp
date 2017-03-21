d2f20f2e5d87b186ee7f93d90f85be5aee22f159
Christoph Maser <christoph.maser@1und1.de>
2017-03-21 19:53:47 +0100

    puppet strings documentation
    annotate code with puppet-strings conforming documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,30 @@
-# systemd_cron
-# ===========================
 #
-
+# systemd_cron
+# Create systemd timer/service to replace cron jobs
+# @param on_calendar systemd timer OnCalendar= definition when to run the 
+#   service as defined in https://www.freedesktop.org/software/systemd/man/systemd.time.html
+# @param command command string for ExecStart= defintion of the service to run
+#   as defined in https://www.freedesktop.org/software/systemd/man/systemd.service.html
+# @param service_description string for Description= defintion of the service
+#   as defined in https://www.freedesktop.org/software/systemd/man/systemd.service.html
+# @param ensure removes the instance if set to false
+# @param timer_description optional string for Description= defintion of the service
+#  as defined in https://www.freedesktop.org/software/systemd/man/systemd.timer.html
+# @param user username to run command User= as defined in 
+#  https://www.freedesktop.org/software/systemd/man/systemd.service.html
+# @param additional_timer_params optional array with lines to append to [Timer] section
+#
+#   example: `'additional_timer_params'   => [ 'RandomizedDelaySec=10' ]`
+# @param additional_service_params optional array with lines to append to [Service] section
+#
+#   example: `'additional_service_params' => [ 'OnFailure=status-email-user@%n.service' ]`
+# @example Usage
+#   `systemd_cron { 'date':
+#     on_calendar         => '*:0/10',
+#     command             => '/bin/date',
+#     service_description => 'Print date',
+#     timer_description   => 'Run date.service every 10 minutes',
+#   }`
 define systemd_cron (
   String            $on_calendar,
   String            $command,
